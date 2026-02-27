c54a291c35b3484633eebfe71c838e90777290b1
godwers <gondes.historia@gmail.com>
2026-02-27 16:15:55 -0300

    making the best neovim configure to ever existgit add nvim

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -1,6 +1,24 @@
-require("config.lazy")
 require("config.keymap")
+require("config.lazy")
+
 vim.cmd.colorscheme("catppuccin-mocha")
+
 require("mason-lspconfig").setup{
 	automatic_enable = true,
 }
+
+local function my_on_attach(bufnr)
+    local api = require "nvim-tree.api"
+
+    local function opts(desc)
+      return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+    end
+
+    -- default mappings)
+
+    -- custom mappings
+    vim.keymap.set("n","<leader>e", api.tree.toggle, opts("Open explorer"))
+end
+require("nvim-tree").setup({
+	on_attach = my_on_attach(),
+})
