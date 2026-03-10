4c2e99155db8af2575c6001a61763053e5b934cd
godwers <gondes.historia@gmail.com>
2026-03-09 22:09:54 -0300

    its not worth it doing out of scratch

--- nvim/dot-config/nvim/init.lua
+++ nvim/dot-config/nvim/init.lua
@@ -19,6 +19,20 @@ local function my_on_attach(bufnr)
     -- custom mappings
     vim.keymap.set("n","<leader>e", api.tree.toggle, opts("Open explorer"))
 end
+
 require("nvim-tree").setup({
 	on_attach = my_on_attach(),
 })
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = vim.tbl_deep_extend('force', capabilities, require('blink.cmp').get_lsp_capabilities({}, false))
+capabilities = vim.tbl_deep_extend('force', capabilities, {
+  textDocument = {
+    foldingRange = {
+      dynamicRegistration = false,
+      lineFoldingOnly = true
+    }
+  }
+})
+
+
--- nvim/dot-config/nvim/lua/plugins/blink.lua
+++ nvim/dot-config/nvim/lua/plugins/blink.lua
@@ -1,22 +1,39 @@
 return {
-	"saghen/blink.cmp",
-	dependencies = { 'rafamadriz/friendly-snippets' },
-	version = '1.*',
+  'saghen/blink.cmp',
+  dependencies = { 'rafamadriz/friendly-snippets' },
+  version = '1.*',
 
-	---@module 'blink.cmp'
-	---@type blink.cmp.Config
-	opts = {
-		keymap = {
-			preset = 'default',
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { 
+	    preset = 'super-tab',
+    },
 
-			['<C-k>'] = { 'select_prev', 'fallback_to_mappings' },
-			['<C-j>'] = { 'select_next', 'fallback_to_mappings' },
-		},
-		fuzzy = { implementation = "lua" },
-		completion = { documentation = { auto_show = true }, },
+    appearance = {
+      nerd_font_variant = 'mono'
+    },
 
+    completion = { documentation = { auto_show = true } },
 
-	},
-	opts_extend = { "sources.default" },
+    sources = {
+      default = { 'lsp', 'path', 'snippets', 'buffer' },
+    },
+
+    signature = { enabled = true },
+    fuzzy = { implementation = "prefer_rust_with_warning" }
+  },
+  opts_extend = { "sources.default" },
 }
-
